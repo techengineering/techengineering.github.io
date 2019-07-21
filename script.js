9538887f02f1e94f13abce62ebff2d75ffcf97ed
techengineering <romarydek2@gmail.com>
2019-07-21 06:12:17 +0300

    mobile vers

---
+++ script.js
@@ -0,0 +1,15 @@
+function slowScroll(id){
+  $('html,body').animate({
+    scrollTop: $(id).offset().top
+  },500);
+}
+
+$(document).on("scroll",function() {
+  if($(window).scrollTop() === 0 )
+   $("header").removeClass("fixed");
+   else
+   $("header").attr("class","fixed");
+})
+function back() {
+  window.location="index.html"
+  alert('yoyoyoyooy');
